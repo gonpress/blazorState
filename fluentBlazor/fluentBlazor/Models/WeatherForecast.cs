851623da1874c1514cd4c057069e641109b7b31a
gonpress <gonpress@naver.com>
2024-08-10 16:13:06 +0900

    initial commit

---
+++ fluentBlazor/fluentBlazor/Models/WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fluentBlazor.Models;
+
+public class WeatherForecast
+{
+    public int Id { get; set; }
+    public required string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public DateTime Date { get; set; }
+    public int TemperatureC { get; set; }
+    [Required]
+    public string? Summary { get; set; }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ fluentBlazor/fluentBlazor/WeatherForecastState.cs
@@ -0,0 +1,20 @@
+﻿using fluentBlazor.Models;
+
+namespace fluentBlazor;
+
+public class WeatherForecastState
+{
+    private WeatherForecast _state = new() { Name = "" };
+    public WeatherForecast State
+    {
+        get => _state;
+        set
+        {
+            _state = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public event Action? OnChange;
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
